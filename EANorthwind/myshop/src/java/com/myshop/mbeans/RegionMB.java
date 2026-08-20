e90d7b16674311afa21da057edbd9a8ca2282e70
Minh-Loan Le <minhloan@cusc.ctu.edu.vn>
2026-08-20 15:15:02 +0700

    new ejb

--- EANorthwind/myshop/src/java/com/myshop/mbeans/RegionMB.java
+++ EANorthwind/myshop/src/java/com/myshop/mbeans/RegionMB.java
@@ -2,7 +2,7 @@ package com.myshop.mbeans;
 
 import com.myshop.ebeans.Region;
 import com.myshop.sbeans.RegionFacadeLocal;
-import jakarta.enterprise.context.SessionScoped;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import java.util.List;
@@ -12,17 +12,48 @@ import java.util.List;
  * @author minhloan
  */
 @Named(value = "regionMB")
-@SessionScoped
+@RequestScoped
 public class RegionMB implements Serializable {
 
     @jakarta.ejb.EJB
     private RegionFacadeLocal regionFacade;
 
+    private Region region = new Region();
     
     public RegionMB() {
+    }
+    
+    public String editRegion(Integer id){
+        region = regionFacade.find(id);
+        return "region";
+    }
+    
+    public String deleteRegion(Integer id){
+        regionFacade.remove(regionFacade.find(id));
+        return "region";
+    }
+    
+    public String saveRegion(){
+        if(regionFacade.find(region.getRegionID())==null){
+            regionFacade.create(region);            
+        }else{
+            regionFacade.edit(region);            
+        }
+        
+        return "region";
     }
     
     public List<Region> showAllRegion(){
         return regionFacade.findAll();
     }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+    
+    
 }
